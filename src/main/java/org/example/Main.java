9bb86321607a67c78a872492e50046ff6e218447
Andrii Bizilia <abizily2000@gmail.com>
2023-06-20 13:33:38 +0300

    dz-4

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+
+        Second second = new Second();
+        int anInt = second.getInt();
+        System.out.println("~~~" + anInt + "~~~");
+    }
+}
